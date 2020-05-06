516f0984dfde26e67e080c4f67dec725712838b0
cmunoz2 <munozc12399@gmail.com>
2020-05-05 20:31:59 -0500

    555

---
+++ Assets/Scripts/FovTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FovTest : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LookForPlayer());
+    }
+
+    IEnumerator LookForPlayer()
+    {
+        while(true)
+        {
+            Debug.Log("Looking for player");
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
